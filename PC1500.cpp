33af07cb7e1f5bafd4d7836cc2339aaa1b99306f
Hanzhi Wang <hanzhi2@gmail.com>
2018-07-30 16:50:01 +1200

    Add basic functionality

--- PC1500.cpp
+++ PC1500.cpp
@@ -1,6 +1,80 @@
 #include "PC1500.h"
+#include "Arduino.h"
 
-PC1500::PC1500() {
+volatile StatusFlag PC1500::statusFlags = static_cast<StatusFlag>(0);
+volatile uint8_t PC1500::numStatusFlagsRead = 0;
+volatile bool PC1500::isReading = false;
 
+uint32_t PC1500::bitsToWrite = 0;
+volatile uint8_t PC1500::numBitsWritten = 0;
+volatile bool PC1500::isWriting = false;
+
+volatile bool PC1500::synced = false;
+
+void PC1500::init(uint8_t clockPin, uint8_t dataPin) {
+    PC1500::clockPin = clockPin;
+    PC1500::dataPin = dataPin;
+    synced = false;
+    attachInterrupt(digitalPinToInterrupt(clockPin), syncIsr, RISING);
+    while(!synced) { }
+    attachInterrupt(digitalPinToInterrupt(clockPin), readWriteIsr, CHANGE);
 }
 
+StatusFlag PC1500::getStatus() {
+    while(isReading) {
+        delay(1);
+    }
+    return statusFlags;
+}
+
+void writeKey(char c) {
+    while(isWriting) {
+        delay(1);
+    }
+    // Repeat the character 4 times;
+    bitsToWrite = c << 24 | c << 16 | c << 8 | c;
+}
+
+
+void PC1500::syncIsr() {
+    delayMicroseconds(1500);
+    if(digitalRead(clockPin) == HIGH) {
+        synced = true;
+    }
+}
+
+void PC1500::readWriteIsr() {
+    delayMicroseconds(50);
+    if(digitalRead(clockPin) == LOW) {
+        if(isWriting) {
+            write();
+            if(numBitsWritten == 32) {
+                isWriting = false;
+            }
+        }
+    } else {
+        if(isReading) {
+            read();
+            if(numStatusFlagsRead == 16) {
+                isReading == false;
+            }
+        }
+    }
+}
+
+void PC1500::read() {
+    numStatusFlagsRead = numStatusFlagsRead % 16;
+    pinMode(dataPin, INPUT);
+    delayMicroseconds(100);
+    statusFlags <<= 1;
+    statusFlags |= digitalRead(dataPin);
+    numStatusFlagsRead++;
+}
+
+void PC1500::write() {
+    numBitsWritten = numBitsWritten % 32;
+    pinMode(dataPin, OUTPUT);
+    delayMicroseconds(100);
+    digitalWrite(dataPin, bitsToWrite & (0x1 << (31 - numBitsWritten)) ? HIGH : LOW);
+    numBitsWritten++;
+}
--- PC1500.h
+++ PC1500.h
@@ -15,6 +15,7 @@
  *   - There are 8 bits of data sent by the keypad, so it is sent twice in the packet
  *   - There are 16 flag bits sent by the master, each corresponding to an LED on the keypad
  * - Can only send a new key every 2 packets to prevent keys being dropped
+ *   - Every key sent is repeated 4 times
 
  * LED flags (MSB first)
  * Zone 1
@@ -52,50 +53,55 @@
  *                     Rows
  */
 
+#include <stdint.h>
+
 #ifndef PC1500_h
 #define PC1500_h
+
+enum StatusFlag
+{
+    ZONE_1  = 1 << 15,
+    ZONE_2  = 1 << 14,
+    ZONE_3  = 1 << 13,
+    ZONE_4  = 1 << 12,
+    ZONE_5  = 1 << 11,
+    ZONE_6  = 1 << 10,
+    ZONE_7  = 1 << 9,
+    ZONE_8  = 1 << 8,
+    READY   = 1 << 7,
+    ARMED   = 1 << 6,
+    MEMORY  = 1 << 5,
+    BYPASS  = 1 << 4,
+    TROUBLE = 1 << 3,
+    BEEP    = 1 << 0
+};
 
 class PC1500 {
 
 public:
-    enum StatusFlag
-    {
-        ZONE_1,
-        ZONE_2,
-        ZONE_3,
-        ZONE_4,
-        ZONE_5,
-        ZONE_6,
-        ZONE_7,
-        ZONE_8,
-        READY,
-        ARMED,
-        MEMORY,
-        BYPASS,
-        TROUBLE,
-        BEEP
-    }
-
-    PC1500();
-
-    void sync();
-    StatusFlag readStatus();
-    void write(char c);
+    static void init(uint8_t clockPin, uint8_t dataPin);
+    static StatusFlag getStatus(void);
+    static void writeKey(char c);
 
 private:
-    uint16_t statusFlags;
-    uint8_t numStatusFlagsRead;
-    bool isReading;
+    static uint8_t clockPin, dataPin;
 
-    uint32_t bitsToWrite;
-    uint8_t numBitsWritten;
-    bool isWriting;
+    static volatile StatusFlag statusFlags;
+    static volatile uint8_t numStatusFlagsRead;
+    static volatile bool isReading;
 
-    bool synced;
+    static uint32_t bitsToWrite;
+    static volatile uint8_t numBitsWritten;
+    static volatile bool isWriting;
 
-    void syncIsr();
-    void readIsr();
-    void writeIsr();
+    static volatile bool synced;
+
+    static void syncIsr(void);
+    static void readWriteIsr(void);
+
+    static void read(void);
+    static void write(void);
+
 };
 
 #endif
